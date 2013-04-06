16cfc06323e0648f4547940462873a8738ccd9d4
Vitor Roberto de Almeida Castro <ra093241@lawrence.lab.ic.unicamp.br>
2013-04-06 16:40:12 -0300

    Update do definitions.h com a sendall()

--- definitions.h
+++ definitions.h
@@ -7,6 +7,14 @@
 #define MAX_TEXT 500
 #define ISBN_LENGTH 14 //Standard is 13, but we must consider the '\0' character.
 
+//Database operations.
+#define ALL_ISBNS_AND_TITLES 0
+#define ISBN_TO_DESCRIPTION 1
+#define ISBN_TO_INFO 2
+#define ALL_BOOKS_INFO 3
+#define CHANGE_STOCK 4
+#define ISBN_TO_STOCK 5
+
 typedef struct _author {
     char name[MAX_NAME];
 } Author;
@@ -20,5 +28,21 @@ typedef struct _book {
     char isbn [ISBN_LENGTH];    
     int stock;
 } book;
+
+//Sends data over the network.
+//Manages the case when the "send()" function does not send all of the data it was asked to send.
+int sendall(int s, char *buf, int *len) {
+	int total = 0; // how many bytes we've sent
+	int bytesleft = *len; // how many we have left to send
+	int n;
+	while(total < *len) {
+		n = send(s, buf+total, bytesleft, 0);
+		if (n == -1) { break; }
+		total += n;
+		bytesleft -= n;
+	}
+	*len = total; // return number actually sent here
+	return n==-1?-1:0; // return -1 on failure, 0 on success
+}
 
 #endif
