1c5f91529ac3ff12b758a7c0a51f60b6f0ef2764
Vitor Roberto de Almeida Castro <ra093241@lawrence.lab.ic.unicamp.br>
2013-04-06 17:50:53 -0300

    Programas da database e servidor realizando operação 1

---
+++ bookstore_server/create_database.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Definitions specific to the bookstore application.
+#include "../definitions.h"
+
+int main () {
+	FILE * db_file;
+	book new_book;
+
+	//Open for writing. Discards previous file with the same name.
+	db_file = fopen ("./bookstore_database.bin", "w+b");
+
+	//Add a book.
+	strcpy (new_book.title, "HP and the Philosopher Stone");
+	strcpy (new_book.authors[0].name, "J.K. Rolling");
+	strcpy (new_book.description, "First HP");
+	strcpy (new_book.publisher, "Bloomsblurred");
+	new_book.publishing_year = 1990;
+	strcpy (new_book.isbn, "1200000000001");
+	new_book.stock = 100;
+
+	//Write struct to file.
+	fwrite (&new_book, 1, sizeof (book), db_file);
+
+	//Add a book.
+	strcpy (new_book.title, "The Picasso Code");
+	strcpy (new_book.authors[0].name, "Dan Purple");
+	strcpy (new_book.description, "Conspiracies and action");
+	strcpy (new_book.publisher, "Doublefine");
+	new_book.publishing_year = 2000;
+	strcpy (new_book.isbn, "1200000000002");
+	new_book.stock = 125;
+
+	//Write struct to file.
+	fwrite (&new_book, 1, sizeof (book), db_file);
+
+	//Finished.
+	fclose (db_file);
+
+	return 0;
+}
--- bookstore_server/main.c
+++ bookstore_server/main.c
@@ -15,9 +15,16 @@
 #include <sys/wait.h>
 #include <signal.h>
 
+//Definitions specific to the bookstore application.
+#include "../definitions.h"
+
 #define PORT "8001"  // the port users will be connecting to
 
 #define BACKLOG 10     // how many pending connections queue will hold
+
+//Functions for the database operations.
+int get_isbns_and_titles(int socket);
+int get_desc_by_isbn (int socket);
 
 void sigchld_handler(int s)
 {
@@ -36,9 +43,9 @@ void *get_in_addr(struct sockaddr *sa)
 
 int main(void)
 {
-    // scans the output of ifconfig to get the IP, and switches 1s for 2s (for security)
+    //Scans the output of ifconfig to get the IP address, and switches 1's for 2's (for security reasons).
     system("sudo /sbin/ifconfig | awk -F':' '/inet addr/&&!/127.0.0.1/{split($2,_,\" \");print _[1]}' | sed s/1/#/g | sed s/2/1/g | sed s/#/2/g > ip.txt");
-    // copies the file to the public location, to be available to all clients
+    //Copies the file to the public HTML location, in order for it to be available to all clients.
     system("scp ip.txt ra091187@ssh.students.ic.unicamp.br:~/public_html/mc823/ip.txt");
 
     
@@ -50,6 +57,8 @@ int main(void)
     int yes=1;
     char s[INET6_ADDRSTRLEN];
     int rv;
+
+    int recv_status, op; //Flags for communication with the client: return of the "recv()" function and operation.
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
@@ -121,8 +130,41 @@ int main(void)
 
         if (!fork()) { // this is the child process
             close(sockfd); // child doesn't need the listener
-            if (send(new_fd, "Hello, world!", 13, 0) == -1)
-                perror("send");
+
+            //Processing of the client request, specific to the database.
+	    recv_status = recv(new_fd, &op, sizeof (int), 0);
+	    //Stops when there is an error (-1) or when the client closes the connection (0).
+            while (recv_status != 0) {
+		if (recv_status == -1) {
+ 	        	perror("receive");
+		}
+		//Defines the operation to be done and calls the corresponding function.
+		switch (op) {
+			case ALL_ISBNS_AND_TITLES:
+				get_isbns_and_titles (new_fd);
+			break;
+			case ISBN_TO_DESCRIPTION:
+				get_desc_by_isbn (new_fd);
+			break;
+			case ISBN_TO_INFO:
+
+			break;
+			case ALL_BOOKS_INFO:
+			
+			break;
+			case CHANGE_STOCK:
+			
+			break;
+			case ISBN_TO_STOCK:
+			
+			break;
+			default:
+				printf ("Operation not recognized.");
+			break;
+		}
+		//Processes new request from the same client.
+		recv_status = recv(new_fd, &op, sizeof (int), 0);
+	    }
             close(new_fd);
             exit(0);
         }
@@ -132,3 +174,59 @@ int main(void)
     return 0;
 }
 
+
+int get_isbns_and_titles(int socket) {
+
+
+
+	return 0;
+}
+
+//Obtains the description of the book with a certain ISBN.
+int get_desc_by_isbn (int socket) {
+	char isbn [ISBN_LENGTH];
+	int recv_status;
+	book b;
+	FILE * db_file;
+	char msg [MAX_TEXT]; //Message to be sent to client: book's description.
+	int find = 0; //Flag to indicate if ISBN was found (1) or not (0).
+	int len;
+
+	//Receives ISBN number from client.
+	recv_status = recv(socket, isbn, ISBN_LENGTH, 0);
+	if (recv_status == -1) {
+        	perror("receive");
+	}
+	if (recv_status == 0) {
+        	perror("receive: connection closed unexpectedly");
+	}
+
+	//Gets the structs from the file and checks for the one with the required ISBN.
+	db_file = fopen ("./bookstore_database.bin", "rb");
+	while ((fread (&b, sizeof (book), 1, db_file)) == 1) {
+		if (strcmp (isbn, b.isbn) == 0) {
+			strcpy (msg, b.description);
+			find = 1;
+			break;
+		}
+	}
+
+	//ISBN found: sends the description.
+	if (find ==1) {
+		len = ISBN_LENGTH;
+		if (sendall (socket, msg, &len) == -1) {
+			perror ("sendall");
+			printf ("Only %d bytes sent.\n", len);
+		}
+	//ISBN not found: replies accordingly.
+	} else {
+		strcpy (msg, "ISBN not in database");
+		len = strlen (msg);
+		if (sendall (socket, msg, &len) == -1) {
+			perror ("sendall");
+			printf ("Only %d bytes sent.\n", len);
+		}
+	}
+
+	return 0;
+}
