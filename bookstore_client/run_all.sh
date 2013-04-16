b00ff49fc1be646e7873cf5f761b980463af1644
Fernando Lucchesi Bastos Jurema <fernandolbastos@gmail.com>
2013-04-16 12:52:23 -0300

    Tirando verificação do run_all

--- bookstore_client/run_all.sh
+++ bookstore_client/run_all.sh
@@ -6,64 +6,10 @@ fi
 
 for i in `seq 1 $times`
 do
-	while true
-	do
-		./client.out 0 2> .tmp
-		if ! grep -q "getaddrinfo: Name or service not known" .tmp
-		then
-			break
-		fi
-	done
-	cat .tmp
-	
-	while true
-	do
-		echo "1200000000001" | ./client.out 1 2> .tmp
-		if ! grep -q "getaddrinfo: Name or service not known" .tmp
-		then
-			break
-		fi
-	done
-	cat .tmp
-	
-	while true
-	do
-		echo "1200000000001" | ./client.out 2 2> .tmp
-		if ! grep -q "getaddrinfo: Name or service not known" .tmp
-		then
-			break
-		fi
-	done
-	cat .tmp
-	
-	while true
-	do
-		echo -e "1200000000001\n8000" | ./client.out 4 bookstore 2> .tmp
-		if ! grep -q "getaddrinfo: Name or service not known" .tmp
-		then
-			break
-		fi
-	done
-	cat .tmp
-	
-	while true
-	do
-		echo "1200000000001" | ./client.out 5 2> .tmp
-		if ! grep -q "getaddrinfo: Name or service not known" .tmp
-		then
-			break
-		fi
-	done
-	cat .tmp
-	
-	while true
-	do
-		./client.out 3 2> .tmp
-		if ! grep -q "getaddrinfo: Name or service not known" .tmp
-		then
-			break
-		fi
-	done
-	cat .tmp
-	rm .tmp
+	./client.out 0
+	echo "1200000000001" | ./client.out 1
+	echo "1200000000001" | ./client.out 2
+	echo -e "1200000000001\n8000" | ./client.out 4 bookstore
+	echo "1200000000001" | ./client.out 5
+	./client.out 3
 done
