6945cc1dd409527d6296b157d7116480e6878f3b
Fernando Lucchesi Bastos Jurema <fernandolbastos@gmail.com>
2013-06-13 01:48:25 -0300

    Simplificando graphs.py; plot em microssegundos

--- Projeto3/logs/graphs.py
+++ Projeto3/logs/graphs.py
@@ -19,7 +19,7 @@ for i in range(6):
     # Read file
     total = open('client_op' + str(i) + '.log', 'r').read()
     total = total.split('\n')
-    total = [int(s) for s in total if len(s) > 0]
+    total = [int(s)/1000 for s in total if len(s) > 0]
     
     # Ignore negative values
     total = [s for s in total if s > 0]
@@ -33,7 +33,7 @@ for i in range(6):
     # Read file
     proc = open('server_op' + str(i) + '.log', 'r').read()
     proc = proc.split('\n')
-    proc = [int(s) for s in proc if len(s) > 0]
+    proc = [int(s)/1000 for s in proc if len(s) > 0]
     
     # Ignore negative values
     proc = [s for s in proc if s > 0]
@@ -65,179 +65,7 @@ N = 6
 ind = np.arange(N)  # the x locations for the groups
 
 
-## Plot1
-# total, proc, comm
-print "Plot 1"
-
-width = 0.3       # the width of the bars
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-rects1 = ax.bar(0.05+ind, totalMeans, width, color='r')#, yerr=totalStd)
-rects2 = ax.bar(0.05+ind+width, procMeans, width, color='y')#, yerr=procStd)
-rects3 = ax.bar(0.05+ind+2*width, commMeans, width, color='b')#, yerr=commStd)
-
-# add some
-ax.set_ylabel('t (useconds)')
-ax.set_title('Time by operation')
-ax.set_xticks(ind+width)
-ax.set_xticklabels( ('OP0', 'OP1', 'OP2', 'OP3', 'OP4', 'OP5') )
-
-ax.legend( (rects1[0], rects2[0], rects3[0]), ('total', 'proc', 'comm') )
-
-def autolabel(rects):
-    # attach some text labels
-    for rect in rects:
-        height = rect.get_height()
-        ax.text(rect.get_x()+rect.get_width()/2., 1.05*height, '%d'%int(height),
-                ha='center', va='bottom')
-
-autolabel(rects1)
-autolabel(rects2)
-autolabel(rects3)
-
-plt.savefig("plot1.png")
-
-
-
-## Plot2
-# total, proc, comm x 2
-print "Plot 2"
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-rects1 = ax.bar(0.05+ind, totalMeans, width, color='r')#, yerr=totalStd)
-rects2 = ax.bar(0.05+ind+width, procMeans, width, color='y')#, yerr=procStd)
-rects3 = ax.bar(0.05+ind+2*width, comm2Means, width, color='b')#, yerr=commStd)
-
-# add some
-ax.set_ylabel('t (useconds)')
-ax.set_title('Time by operation')
-ax.set_xticks(ind+width)
-ax.set_xticklabels( ('OP0', 'OP1', 'OP2', 'OP3', 'OP4', 'OP5') )
-
-ax.legend( (rects1[0], rects2[0], rects3[0]), ('total', 'proc', 'comm x 2') )
-
-def autolabel(rects):
-    # attach some text labels
-    for rect in rects:
-        height = rect.get_height()
-        ax.text(rect.get_x()+rect.get_width()/2., 1.05*height, '%d'%int(height),
-                ha='center', va='bottom')
-
-autolabel(rects1)
-autolabel(rects2)
-autolabel(rects3)
-
-plt.savefig("plot2.png")
-
-
-## Plot3
-# everything
-print "Plot 3"
-
-width = 0.2       # the width of the bars
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-rects1 = ax.bar(0.05+ind, totalMeans, width, color='r')#, yerr=totalStd)
-rects2 = ax.bar(0.05+ind+width, procMeans, width, color='y')#, yerr=procStd)
-rects3 = ax.bar(0.05+ind+2*width, commMeans, width, color='b')#, yerr=commStd)
-rects4 = ax.bar(0.05+ind+3*width, comm2Means, width, color='g')#, yerr=comm2Std)
-
-# add some
-ax.set_ylabel('t (useconds)')
-ax.set_title('Time by operation')
-ax.set_xticks(ind+width)
-ax.set_xticklabels( ('OP0', 'OP1', 'OP2', 'OP3', 'OP4', 'OP5') )
-
-ax.legend( (rects1[0], rects2[0], rects3[0], rects4[0]), ('total', 'proc', 'comm', 'comm x 2') )
-
-def autolabel(rects):
-    # attach some text labels
-    for rect in rects:
-        height = rect.get_height()
-        ax.text(rect.get_x()+rect.get_width()/2., 1.05*height, '%d'%int(height),
-                ha='center', va='bottom')
-
-autolabel(rects1)
-autolabel(rects2)
-autolabel(rects3)
-autolabel(rects4)
-
-plt.savefig("plot3.png")
-
-
-## Plot4
-# total, proc, comm
-print "Plot 4"
-
-width = 0.3       # the width of the bars
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-rects1 = ax.bar(0.05+ind, totalMeans, width, color='r', yerr=totalStd)
-rects2 = ax.bar(0.05+ind+width, procMeans, width, color='y', yerr=procStd)
-rects3 = ax.bar(0.05+ind+2*width, commMeans, width, color='b', yerr=commStd)
-
-# add some
-ax.set_ylabel('t (useconds)')
-ax.set_title('Time by operation')
-ax.set_xticks(ind+width)
-ax.set_xticklabels( ('OP0', 'OP1', 'OP2', 'OP3', 'OP4', 'OP5') )
-
-ax.legend( (rects1[0], rects2[0], rects3[0]), ('total', 'proc', 'comm') )
-
-def autolabel(rects):
-    # attach some text labels
-    for rect in rects:
-        height = rect.get_height()
-        ax.text(rect.get_x()+rect.get_width()/2., 1.05*height, '%d'%int(height),
-                ha='center', va='bottom')
-
-autolabel(rects1)
-autolabel(rects2)
-autolabel(rects3)
-
-plt.savefig("plot4.png")
-
-
-
-## Plot5
-# total, proc, comm x 2
-print "Plot 5"
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-rects1 = ax.bar(0.05+ind, totalMeans, width, color='r', yerr=totalStd)
-rects2 = ax.bar(0.05+ind+width, procMeans, width, color='y', yerr=procStd)
-rects3 = ax.bar(0.05+ind+2*width, comm2Means, width, color='b', yerr=commStd)
-
-# add some
-ax.set_ylabel('t (useconds)')
-ax.set_title('Time by operation')
-ax.set_xticks(ind+width)
-ax.set_xticklabels( ('OP0', 'OP1', 'OP2', 'OP3', 'OP4', 'OP5') )
-
-ax.legend( (rects1[0], rects2[0], rects3[0]), ('total', 'proc', 'comm x 2') )
-
-def autolabel(rects):
-    # attach some text labels
-    for rect in rects:
-        height = rect.get_height()
-        ax.text(rect.get_x()+rect.get_width()/2., 1.05*height, '%d'%int(height),
-                ha='center', va='bottom')
-
-autolabel(rects1)
-autolabel(rects2)
-autolabel(rects3)
-
-plt.savefig("plot5.png")
-
-
-## Plot6
-# everything
-print "Plot 6"
+print "Plotting..."
 
 width = 0.2       # the width of the bars
 
@@ -268,4 +96,4 @@ autolabel(rects2)
 autolabel(rects3)
 autolabel(rects4)
 
-plt.savefig("plot6.png")
+plt.savefig("plot.png")
